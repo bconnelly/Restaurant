ecf3262ae5659f0e7fd78bf303873ec6c0d26429
Bryan <bryan.p.connelly@gmail.com>
2023-03-06 19:55:21 -0700

    testing jenkins github integration

--- RestaurantService/src/main/java/com/fullstack/restaurantservice/RestaurantServiceApplication.java
+++ RestaurantService/src/main/java/com/fullstack/restaurantservice/RestaurantServiceApplication.java
@@ -77,3 +77,5 @@ public class RestaurantServiceApplication extends SpringBootServletInitializer {
         return ResponseEntity.status(HttpStatus.OK).body(restaurantLogic.bootCustomer(firstName));
     }
 }
+
+//testing github jenkins integration
